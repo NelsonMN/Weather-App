4ee4f7374a35ca4257829e038af3ce60fa3f2dd3
Nelson <nelson.mieszkalski@gmail.com>
2022-05-10 16:13:44 -0400

    Added some HTML and style

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-async function fetchWeatherData(city) {
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84f293469f95029376a49c8dbfc2b1d4`, {mode: 'cors'});
-        const weatherData = await response.json()
-        console.log(weatherData) 
-    } catch (response) {
-        console.log(response)
-    }
-}
+// Fetch Weather using API
 
-fetchWeatherData(Toronto);
+async function fetchWeatherData(city) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84f293469f95029376a49c8dbfc2b1d4`, {mode: 'cors'});
+    const weatherData = await response.json();
+    if (weatherData.cod == '404') {
+        // Handle error here
+    } else {
+        return weatherData
+    }
+};
