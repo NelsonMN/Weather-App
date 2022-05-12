4fc5f71922a76abcf2f8ae8c1a888feb36c007d3
Nelson <nelson.mieszkalski@gmail.com>
2022-05-12 12:45:44 -0400

    Loaded Webpack

---
+++ src/scripts/index.js
@@ -0,0 +1,28 @@
+import "./styles.css"
+import updateAppUI from "./ui";
+import { fetchData, processData } from "./weather";
+
+const search = document.getElementById("submit-button");
+const input = document.getElementById("search");
+
+search.addEventListener("click", (e) => {
+  e.preventDefault();
+  let searchInput = input.value;
+  const initialOutput = fetchData(searchInput);
+  initialOutput
+    .then((data) => {
+      const formattedData = processData(data);
+      updateAppUI(formattedData);
+      input.value = "";
+    })
+    .catch(() => {
+      input.value = "";
+      return;
+    });
+});
+
+// Load Toronto
+
+fetchData("Toronto").then((data) => {
+  updateAppUI(processData(data));
+});
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/scripts/index.js',
+          './src/scripts/ui.js',
+          './src/scripts/weather.js'
+],
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+     {
+       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+       type: 'asset/resource',
+     },
+    ],
+  },
+};
+
